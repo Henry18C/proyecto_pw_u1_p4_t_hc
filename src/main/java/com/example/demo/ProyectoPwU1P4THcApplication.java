b245f23255c23708eb00316168de64b333ac0f2e
Henry17C <hicoyago@uce.edu.ec>
2023-05-14 08:49:12 -0500

    primer commit

---
+++ src/main/java/com/example/demo/ProyectoPwU1P4THcApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPwU1P4THcApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPwU1P4THcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("primer commit");
+	}
+
+}
